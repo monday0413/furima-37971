b9013e3ebf1d2447ab3a76f0700d8ff85ec81600
monday0413 <akane.takahashi.3140@gmail.com>
2022-05-17 21:07:18 +0900

    ログイン機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 
@@ -11,13 +12,16 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      # username == 'akane' && password == '0413'
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      username == 'akane' && password == '0413'
+      # username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
   
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname],[:family_name],[:first_name],[:family_name_kana],[:first_name_kana],[:birth_day])
+    
+     devise_parameter_sanitizer.permit(
+       :sign_up, keys: [:nickname,:family_name,:first_name,:family_name_kana,:first_name_kana,:birth_day]
+     )
   end
 
 end
