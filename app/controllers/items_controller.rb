9f98f0b63ed096bb8ac2b4f8f9d84201619ec7db
monday0413 <akane.takahashi.3140@gmail.com>
2022-05-19 02:13:03 +0900

    activehashの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,32 @@
 class ItemsController < ApplicationController
- 
+
+  before_action :move_to_index, except: [:index]
+
   def new
-    @item = Item.new
+  end
+
+  def create
+    
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+
+  private
+
+  def item_params
+    params.permit(:image, :item_name, :explanation, :category_id, :condition_id, :send_charge_id, :prefecture_id, :days_ship_id,:price).merge(user_id: current_user.id)
+  end
+  
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,20 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+      belongs_to :category
+      belongs_to :condition
+      belongs_to :send_charge
+      belongs_to :prefecture
+      belongs_to :days_ship
+      
+      has_one_attached :image
+      VALID_PRICE_REGEX = /\A[0-9]+\z/.freeze
 
-  has_one_attached :image
-
+      validates :category_id, numericality: { other_than: 1 } 
+      validates :condition_id, numericality: { other_than: 1 } 
+      validates :send_charge_id, numericality: { other_than: 1 } 
+      validates :prefecture_id, numericality: { other_than: 1 } 
+      validates :days_ship_id, numericality: { other_than: 1 } 
   validates :image, presence: true
-  
+ 
+  validates :price, numericality: { in: 300..9999999 },format: { with: VALID_PRICE_REGEX }
 end
