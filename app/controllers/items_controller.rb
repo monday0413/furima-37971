bacd176d91074b75057d5e125e328d6e632dc514
monday0413 <akane.takahashi.3140@gmail.com>
2022-05-22 18:45:52 +0900

    updateアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,6 +26,16 @@ def edit
   @item = Item.find(params[:id])
 end
 
+def update
+  item = Item.find(params[:id])
+   if item.update(item_params)
+    redirect_to root_path
+  else
+    @item = Item.find(params[:id])
+    render :edit
+  end
+end
+
   private
 
   def item_params
