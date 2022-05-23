0b04fecf4fcda8fa652a6123a4b01e6a5cd7ec2e
monday0413 <akane.takahashi.3140@gmail.com>
2022-05-23 15:32:14 +0900

    価格の表示、記述の削減

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  before_action :move_to_index, except: [:index, :show, :new, :create]
-  before_action :item_set, only: [:show, :edit, :update]
+  before_action :item_set, only: [:show, :edit, :update, :new, :create]
+  before_action :move_to_index, only: [:edit, :update]
+
   def new
     @item = Item.new
   end
@@ -40,12 +41,11 @@ class ItemsController < ApplicationController
                                  :days_ship_id, :price).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    @item = Item.find(params[:id])
-    redirect_to action: :index unless @item.user_id == current_user.id
-  end
-
   def item_set
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to action: :index unless @item.user_id == current_user.id
   end
 end
