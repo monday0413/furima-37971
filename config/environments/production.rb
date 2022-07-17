e3c545d0e1507b7131550e6252e027b10a669d25
monday0413 <akane.takahashi.3140@gmail.com>
2022-07-18 01:46:56 +0900

    heroku対応

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,9 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  ActionCable.server.config.disable_request_forgery_protection = true
+  config.action_cable.url = "wss://furoma-37971.herokuapp.com/cable" 
+  config.action_cable.allowed_request_origins = ['https://furoma-37971.herokuapp.com', 'http://furoma-37971.herokuapp.com']
+
 end
